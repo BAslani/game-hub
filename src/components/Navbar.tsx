f2491221a58aee7971f2d36b4c152bb0a7c1c890
Behzad Aslani <behzadaslani80@gmail.com>
2023-12-09 12:56:35 +0330

    feat:[WIP] added dark mode toggle button to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { BiSearch } from 'react-icons/bi'
+import DarkModeToggleButton from './DarkModeToggleButton'
 
 const Navbar = () => {
   return (
     <nav className='my-4 flex mx-auto flex-row gap-4 items-center max-w-7xl justify-between'>
       <div className='w-12 h-12'>
-        <img src='/public/images/favicon.ico' alt='' />
+        <img src='/public/images/favicon.ico' alt='logo' />
       </div>
       <form className='flex flex-row items-center gap-4 bg-slate-200 py-2 px-2 rounded-full w-full'>
         <BiSearch />
@@ -16,7 +17,7 @@ const Navbar = () => {
           className='bg-transparent outline-none'
         />
       </form>
-      <div>dark mode</div>
+      <DarkModeToggleButton />
     </nav>
   )
 }
