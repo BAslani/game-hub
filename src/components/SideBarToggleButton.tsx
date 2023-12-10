0cf04a4939eef33f2d5378b2263555fc1eca1f3c
Behzad Aslani <behzadaslani80@gmail.com>
2023-12-10 12:05:50 +0330

    feat: sidebars responsiveness implemented

--- src/components/SideBarToggleButton.tsx
+++ src/components/SideBarToggleButton.tsx
@@ -1,8 +1,12 @@
 import { FaBars } from 'react-icons/fa'
+import { useAppDispatch } from '../store'
+import { toggleSideBar } from '../features/sideBar/sideBarSlice'
 
 const SideBarToggleButton = () => {
+  const dispatch = useAppDispatch()
+
   return (
-    <button className='md:hidden'>
+    <button className='md:hidden' onClick={() => dispatch(toggleSideBar())}>
       <FaBars className='text-3xl' />
     </button>
   )
